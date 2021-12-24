4aa276a832925b09dffce10efc7497b24fe7d5f0
Siu <siuc@protonmail.com>
2021-12-24 12:22:13 -0500

    moving content from old files to create-my-react files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="app">
+        <div id="quote-box">
+          <div id="text">
+            <i className="fa-xs fas fa-quote-left quotation-mark"></i>
+            {"QUOTE"}
+            <i className="fa-xs fas fa-quote-right quotation-mark"></i>
+          </div>
+          <div id="author">—{"Author"}</div>
+          <footer>
+            <div className="flex-container">
+              <a
+                id="tweet-quote"
+                href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${"QUOTE"}"%0—${"author"}`}
+                target="_blank"
+              >
+                <i className="fab fa-lg fa-twitter-square"></i>
+              </a>
+              <a
+                href={`https://t.me/share/url?url="${"QUOTE"}"&text=—${"author"}`}
+                target="_blank"
+              >
+                <i className="fa-lg fab fa-telegram"></i>
+              </a>
+              <a
+                href={`mailto:?subject=Quote of the Day&body="${"QUOTE"}"\n
+      —${"Author"}`}
+                target="_blank"
+              >
+                <i className="fas fa-lg fa-envelope-square"></i>
+              </a>
+            </div>
+            <button
+              id="new-quote"
+              onClick={() => console.log("newquote")}
+              className="btn btn-default btn-dark"
+            >
+              New Quote
+            </button>
+          </footer>
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
